f938d1a7c012f77950d0bb3bfb43152d0773210a
iaiapps <ikromudin.ai@gmail.com>
2024-01-20 23:12:42 +0700

    fix type gaji

--- resources/views/finance/type/edit.blade.php
+++ resources/views/finance/type/edit.blade.php
@@ -1,6 +1,6 @@
 @extends('layouts.app')
 
-@section('title', 'Edit Data Gaji Pokok')
+@section('title', 'Edit Data Type Gaji')
 @section('content')
     <div class="card p-3">
         <form action="{{ route('type.update', $type->id) }}" method="POST">
@@ -10,23 +10,23 @@
                 <div class="row">
                     <div class="col">
                         <div class="mb-3">
-                            <label class="form-label" for="type_gaji">Nama Jabatan </label>
-                            <input class="form-control bg-light" type="text" id="type_gaji" name="type_gaji"
-                                placeholder="type" value="{{ $type->type_gaji }}" readonly />
+                            <label class="form-label" for="type">Nama </label>
+                            <input class="form-control bg-light" type="text" id="type" name="type"
+                                placeholder="type" value="{{ $type->type }}" readonly />
                         </div>
                     </div>
                     <div class="col">
                         <div class="mb-3">
-                            <label class="form-label" for="nama_gaji">Nama Jabatan </label>
-                            <input class="form-control" type="text" id="nama_gaji" name="nama_gaji" placeholder="nama "
-                                value="{{ $type->nama_gaji }}" />
+                            <label class="form-label" for="nama">Nama Jabatan </label>
+                            <input class="form-control" type="text" id="nama" name="nama" placeholder="nama "
+                                value="{{ $type->nama }}" />
                         </div>
                     </div>
                     <div class="col">
                         <div class="mb-3">
-                            <label class="form-label" for="besar_gaji">Gaji Pokok </label><input class="form-control"
-                                type="number" id="besar_gaji" name="besar_gaji" placeholder="besarnya"
-                                value="{{ $type->besar_gaji }}" />
+                            <label class="form-label" for="besarnya">Gaji Pokok </label><input class="form-control"
+                                type="number" id="besarnya" name="besarnya" placeholder="besarnya"
+                                value="{{ $type->besarnya }}" />
                         </div>
                     </div>
                 </div>
